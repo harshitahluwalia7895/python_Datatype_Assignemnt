1b8d1ec0756a05315ae86b40b31564de633a55b2
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-23 22:01:20 +0530

    final commit

---
+++ answer3.py
@@ -0,0 +1,10 @@
+
+#Answer 3 
+	my_list=[]
+        my_list.append('Google')
+        my_list.append('facebook')
+        my_list.append('Microsoft')
+        my_list.append('Apple')
+        my_list.append('Tesla')
+        print(my_list)
+	print(my_list.count('Google'))
