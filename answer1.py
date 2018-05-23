d26303e0ef65ca077f80408ab4c36778afc1c8a3
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-23 21:45:07 +0530

    final commit

---
+++ answer1.py
@@ -0,0 +1,9 @@
+#Q.1- Create a list with user defined inputs. 
+
+new_list=[]
+choice='y'
+while choice=='y' || 'Y':
+	element=input("Enter a element in the List: ")
+	new_list.append(element)
+	choice=input("Do you want to enter more? y/n: ")
+print(new_list)
