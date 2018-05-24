abc03b232dc89d0df81b900207daea2acdce9c13
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-24 16:53:05 +0530

    Final Commit

--- answer2.py
+++ answer2.py
@@ -1,13 +1,13 @@
 Q2.Add the following list to above created list(list created in q1):
 ['google','apple','facebook','microsoft','tesla']
 
-	new_list=[]
-	choice='y'
-	while choice=='y':
-		element=input("Enter a element in the List: ")
-		new_list.append(element)
-		choice=input("Do you want to enter more? y/n: ")
-	print(new_list)
-	second_list=['google','apple','facebook','microsoft','tesla']
-	new_list.extend(second_list)
-	print(new_list)
+new_list=[]
+choice='y'
+while choice=='y':
+	element=input("Enter a element in the List: ")
+	new_list.append(element)
+	choice=input("Do you want to enter more? y/n: ")
+print(new_list)
+second_list=['google','apple','facebook','microsoft','tesla']
+new_list.extend(second_list)
+print(new_list)
